c28a8080de2914cb0aa747923a53d4119a1609fc
Bruno Militzer <brunomilitzer.work@gmail.com>
2021-08-11 22:54:22 +0100

    Refactored Beer Controller to Reactive Implemenation

--- reactive-brewery/src/main/java/com/brunomilitzer/restbrewery/web/controller/BeerController.java
+++ reactive-brewery/src/main/java/com/brunomilitzer/restbrewery/web/controller/BeerController.java
@@ -6,11 +6,11 @@ import com.brunomilitzer.restbrewery.web.model.BeerPagedList;
 import com.brunomilitzer.restbrewery.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
@@ -28,11 +28,11 @@ public class BeerController {
     private final BeerService beerService;
 
     @GetMapping( produces = { "application/json" }, path = "beer" )
-    public ResponseEntity<BeerPagedList> listBeers( @RequestParam( value = "pageNumber", required = false ) Integer pageNumber,
-                                                    @RequestParam( value = "pageSize", required = false ) Integer pageSize,
-                                                    @RequestParam( value = "beerName", required = false ) final String beerName,
-                                                    @RequestParam( value = "beerStyle", required = false ) final BeerStyleEnum beerStyle,
-                                                    @RequestParam( value = "showInventoryOnHand", required = false ) Boolean showInventoryOnHand ) {
+    public ResponseEntity<Mono<BeerPagedList>> listBeers( @RequestParam( value = "pageNumber", required = false ) Integer pageNumber,
+                                                          @RequestParam( value = "pageSize", required = false ) Integer pageSize,
+                                                          @RequestParam( value = "beerName", required = false ) final String beerName,
+                                                          @RequestParam( value = "beerStyle", required = false ) final BeerStyleEnum beerStyle,
+                                                          @RequestParam( value = "showInventoryOnHand", required = false ) Boolean showInventoryOnHand ) {
 
         if ( showInventoryOnHand == null ) {
             showInventoryOnHand = false;
@@ -48,28 +48,28 @@ public class BeerController {
 
         final BeerPagedList beerList = this.beerService.listBeers( beerName, beerStyle, PageRequest.of( pageNumber, pageSize ), showInventoryOnHand );
 
-        return new ResponseEntity<>( beerList, HttpStatus.OK );
+        return ResponseEntity.ok( Mono.just( beerList ) );
     }
 
     @GetMapping( "beer/{beerId}" )
-    public ResponseEntity<BeerDto> getBeerById( @PathVariable( "beerId" ) final UUID beerId,
-                                                @RequestParam( value = "showInventoryOnHand", required = false ) Boolean showInventoryOnHand ) {
+    public ResponseEntity<Mono<BeerDto>> getBeerById( @PathVariable( "beerId" ) final UUID beerId,
+                                                      @RequestParam( value = "showInventoryOnHand", required = false ) Boolean showInventoryOnHand ) {
 
         if ( showInventoryOnHand == null ) {
             showInventoryOnHand = false;
         }
 
-        return new ResponseEntity<>( this.beerService.getById( beerId, showInventoryOnHand ), HttpStatus.OK );
+        return ResponseEntity.ok( Mono.just( this.beerService.getById( beerId, showInventoryOnHand ) ) );
     }
 
     @GetMapping( "beerUpc/{upc}" )
-    public ResponseEntity<BeerDto> getBeerByUpc( @PathVariable( "upc" ) final String upc ) {
+    public ResponseEntity<Mono<BeerDto>> getBeerByUpc( @PathVariable( "upc" ) final String upc ) {
 
-        return new ResponseEntity<>( this.beerService.getByUpc( upc ), HttpStatus.OK );
+        return ResponseEntity.ok( Mono.just( this.beerService.getByUpc( upc ) ) );
     }
 
     @PostMapping( path = "beer" )
-    public ResponseEntity saveNewBeer( @RequestBody @Validated final BeerDto beerDto ) {
+    public ResponseEntity<Void> saveNewBeer( @RequestBody @Validated final BeerDto beerDto ) {
 
         final BeerDto savedBeer = this.beerService.saveNewBeer( beerDto );
 
@@ -81,9 +81,9 @@ public class BeerController {
     }
 
     @PutMapping( "beer/{beerId}" )
-    public ResponseEntity updateBeerById( @PathVariable( "beerId" ) final UUID beerId, @RequestBody @Validated final BeerDto beerDto ) {
+    public ResponseEntity<Void> updateBeerById( @PathVariable( "beerId" ) final UUID beerId, @RequestBody @Validated final BeerDto beerDto ) {
 
-        return new ResponseEntity<>( this.beerService.updateBeer( beerId, beerDto ), HttpStatus.NO_CONTENT );
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping( "beer/{beerId}" )
@@ -91,7 +91,7 @@ public class BeerController {
 
         this.beerService.deleteBeerById( beerId );
 
-        return new ResponseEntity<>( HttpStatus.NO_CONTENT );
+        return ResponseEntity.noContent().build();
     }
 
 }
---
+++ reactive-brewery/src/test/java/com/brunomilitzer/restbrewery/web/controller/BeerControllerTest.java
@@ -0,0 +1,85 @@
+package com.brunomilitzer.restbrewery.web.controller;
+
+import com.brunomilitzer.restbrewery.bootstrap.BeerLoader;
+import com.brunomilitzer.restbrewery.services.BeerService;
+import com.brunomilitzer.restbrewery.web.model.BeerDto;
+import com.brunomilitzer.restbrewery.web.model.BeerPagedList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@WebFluxTest( BeerController.class )
+class BeerControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private BeerService beerService;
+
+    private BeerDto validBeer;
+
+    @BeforeEach
+    void setUp() {
+
+        this.validBeer = BeerDto.builder().beerName( "Test beer" )
+                .beerStyle( "PALE_ALE" )
+                .upc( BeerLoader.BEER_1_UPC ).build();
+    }
+
+    @Test
+    void getBeerByUpc() {
+
+        given( this.beerService.getByUpc( any() ) )
+                .willReturn( this.validBeer );
+
+        this.webTestClient.get().uri( "/api/v1/beerUpc/" + this.validBeer.getUpc() )
+                .accept( MediaType.APPLICATION_JSON )
+                .exchange().expectStatus().isOk()
+                .expectBody( BeerDto.class )
+                .value( BeerDto::getBeerName, equalTo( this.validBeer.getBeerName() ) );
+
+    }
+
+    @Test
+    public void listBeers() {
+
+        final List<BeerDto> beerList = Arrays.asList( this.validBeer );
+
+        final BeerPagedList beerPagedList = new BeerPagedList( beerList, PageRequest.of( 1, 1 ), beerList.size() );
+        given( this.beerService.listBeers( any(), any(), any(), any() ) ).willReturn( beerPagedList );
+
+        this.webTestClient.get().uri( "/api/v1/beer" ).accept( MediaType.APPLICATION_JSON )
+                .exchange().expectStatus().isOk()
+                .expectBody( BeerPagedList.class );
+    }
+
+    @Test
+    void getBeerById() {
+
+        final UUID beerId = UUID.randomUUID();
+        given( this.beerService.getById( any(), any() ) )
+                .willReturn( this.validBeer );
+
+        this.webTestClient.get().uri( "/api/v1/beer/" + beerId )
+                .accept( MediaType.APPLICATION_JSON )
+                .exchange().expectStatus().isOk()
+                .expectBody( BeerDto.class )
+                .value( BeerDto::getBeerName, equalTo( this.validBeer.getBeerName() ) );
+
+    }
+
+}
